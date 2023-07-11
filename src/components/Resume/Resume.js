ae8e9c6890ecb9e0aff7cfeab44ae5ee1d4f38a2
Sarthak Sharma <89703198+sarthak5101@users.noreply.github.com>
2023-07-11 11:09:37 +0530

    Update Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -6,6 +6,7 @@ import pdf from "../../Assets/../Assets/Sarthak Sharma Resume.pdf"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function Resume() {
